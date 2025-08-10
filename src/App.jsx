49ac988d39d9fbfe45341e200d235b9102dfef5a
Ossi05 <77546709+Ossi05@users.noreply.github.com>
2025-08-10 20:59:21 +0300

    Initialize project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+function App() {
+  return <h2>Hello</h2>
+}
+
+export default App
