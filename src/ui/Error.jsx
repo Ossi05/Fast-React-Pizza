79adba62a240c2fe2cc9df252440dc49dd9ee08e
Ossi05 <77546709+Ossi05@users.noreply.github.com>
2025-08-14 17:01:32 +0300

    Style Error page

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -5,9 +5,11 @@ function Error() {
 	const error = useRouteError();
 	console.log(error);
 	return (
-		<div>
-			<h1>Something went wrong 😢</h1>
-			<p>
+		<div className="py-3 px-4">
+			<h1 className="text-2xl text-red-500 mb-1">
+				Something went wrong 😢
+			</h1>
+			<p className="font-bold">
 				{error.status} {error.data || error.message}
 			</p>
 			<LinkButton to="-1">&larr; Go back</LinkButton>
