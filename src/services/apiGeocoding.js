a639e94d28569f1ccf2657ab92278df8f2a3334c
Ossi05 <77546709+Ossi05@users.noreply.github.com>
2025-08-14 17:18:25 +0300

    Add and configure Redux. Configure userSlice

--- src/services/apiGeocoding.js
+++ src/services/apiGeocoding.js
@@ -1,9 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 export async function getAddress({ latitude, longitude }) {
 	const res = await fetch(
-		`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`,
+		`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`
 	);
 	if (!res.ok) throw Error("Failed getting address");
 
 	const data = await res.json();
 	return data;
 }
+
+const initialState = {
+	username: "",
+};
+
+const userSlice = createSlice({
+	name: "user",
+	initialState,
+	reducers: {
+		updateName(state, action) {
+			state.username = action.payload;
+		},
+	},
+});
+
+export const { updateName } = userSlice.actions;
+export default userSlice.reducer;
