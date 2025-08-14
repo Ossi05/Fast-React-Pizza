d94f674ab42215a56ef401d3f56d26f2fa9ef3b8
Ossi05 <77546709+Ossi05@users.noreply.github.com>
2025-08-14 10:17:21 +0300

    Style Menu page. Update Button component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,18 +1,29 @@
 import { Link } from "react-router-dom";
 
-export default function Button({ children, to, className = "", ...props }) {
-	const classes = `inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold tracking-wide focus:ring-offset-2 text-stone-800 uppercase focus:bg-yellow-300 transition-colors duration-300 hover:cursor-pointer focus:ring-yellow-300 focus:ring hover:bg-yellow-300 focus:outline-none disabled:cursor-not-allowed sm:px-6 sm:py-4 ${className}`;
+export default function Button({
+	children,
+	type,
+	to,
+	className = "",
+	...props
+}) {
+	const baseStyles = `inline-block rounded-full bg-yellow-400 font-semibold tracking-wide focus:ring-offset-2 text-stone-800 uppercase focus:bg-yellow-300 transition-colors duration-300 hover:cursor-pointer focus:ring-yellow-300 focus:ring hover:bg-yellow-300 focus:outline-none disabled:cursor-not-allowed ${className} `;
+
+	const styles = {
+		primary: baseStyles + "px-4 py-3 md:px-6 md:py-4",
+		small: baseStyles + "px-4 py-2 md:px-5 md:py-2.5 text-xs",
+	};
 
 	if (to) {
 		return (
-			<Link className={classes} to={to} {...props}>
+			<Link className={styles[type]} to={to} {...props}>
 				{children}
 			</Link>
 		);
 	}
 
 	return (
-		<button className={classes} {...props}>
+		<button className={styles[type]} {...props}>
 			{children}
 		</button>
 	);
