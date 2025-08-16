757c4b6c49c33d6cf5cd1a26dd65428409250157
Ossi05 <77546709+Ossi05@users.noreply.github.com>
2025-08-16 13:22:22 +0300

    Add UpdateItemQuantity component and integrate with CartItem and MenuItem

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -3,6 +3,7 @@ import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
 import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
 import DeleteItem from "../cart/DeleteItem";
+import UpdateItemQuantity from "../cart/UpdateItemQuantity";
 
 function MenuItem({ pizza }) {
 	const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
@@ -44,7 +45,10 @@ function MenuItem({ pizza }) {
 
 					{!soldOut &&
 						(isInCart ? (
-							<DeleteItem pizzaId={id} />
+							<div className="flex items-center gap-3 sm:gap-8">
+								<UpdateItemQuantity pizzaId={id} />
+								<DeleteItem pizzaId={id} />
+							</div>
 						) : (
 							<Button type="small" onClick={handleAddToCart}>
 								Add To Cart
